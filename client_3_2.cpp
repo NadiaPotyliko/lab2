6de112acbf2ec05fabf5c330e7d6b0f554d62355
NadiaPotyliko <82584263+NadiaPotyliko@users.noreply.github.com>
2021-12-10 09:36:28 +0300

    Update client_3_2.cpp

--- client_3_2.cpp
+++ client_3_2.cpp
@@ -1,99 +1,79 @@
-﻿#include <cstdio>
-#include <stdlib.h>
+#define WINVER 0x0502
 #include <windows.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
-#pragma warning(disable : 4996)
-#define KILOBYTE 1024
-#define BUFFER_SIZE (25 * KILOBYTE)
-
-
-
-
-using namespace std;
-int showMenu() {
-	printf("1. - Поместить сообщение в почтовый ящик \n");
-	printf("2. - Выход\n");
-	int c = 1;
-	cin >> c;
-	fscanf(stdin, "%*c");
-	return c;
-}
-int showMenu();
+#include <cstring>
+#include <sddl.h>
+#include <conio.h>
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	string name, name1;
-	cout << "Введите имя почтового ящика:";
-	cin >> name;
-	name1 = "\\\\.\\mailslot\\" + name;
-	LPCSTR lpstrHash = name1.c_str();
-	HANDLE hf = NULL, hsl = NULL;
-	hsl = CreateMailslot(lpstrHash, 0, 0, NULL);
-	if (hsl != INVALID_HANDLE_VALUE)
-	{
-		cout << "Mailslot создан" << endl;
-	}
+  BOOL   fReturnCode; 
+  DWORD  cbMessages;
+  DWORD  cbMsgNumber;
+  HANDLE hMailslot;
+  LPSTR  lpszMailslotName = "\\\\.\\mailslot\\$Channel$";
+  char   szBuf[512];
+  DWORD  cbRead;
+  SECURITY_ATTRIBUTES attributes;
+  attributes.nLength = sizeof(SECURITY_ATTRIBUTES);
+  attributes.bInheritHandle = TRUE;
+  attributes.lpSecurityDescriptor = NULL;
+  
+  printf("Mailslot SERVER\n");
+  
+  hMailslot = CreateMailslot(
+    lpszMailslotName, 0,
+    MAILSLOT_WAIT_FOREVER,&attributes);
+    
+  if(hMailslot == INVALID_HANDLE_VALUE)
+  {
+    fprintf(stdout,"CreateMailslot: Error %ld\n", 
+      GetLastError());
+    getch();
+    return 0;
+  }
 
-		hf = CreateFile(lpstrHash, GENERIC_WRITE,
-			FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-		if (hf != INVALID_HANDLE_VALUE)
-		{
-			cout << "Файл создан" << endl;
-		}
-	
-	cout << "Введите имя для семафора:";
-	cin >> name;
-	LPCSTR lpstrHas = name.c_str();
-	HANDLE hSemaphore;
-	hSemaphore = CreateSemaphore(
+  fprintf(stdout,"Mailslot created\n"); 
 
-		NULL,	// нет атрибута
-		0,	// начальное состояние
-		100,	// максимальное состояние
-		lpstrHas	// без имени
-	);
-	if (hSemaphore == NULL)
-	{
-		printf("CreateSemaphore error: %d\n", GetLastError());
-		hSemaphore = OpenSemaphore(NULL, FALSE, lpstrHas);
-		if (hSemaphore == NULL)
-			printf("OpenSemaphore error: %d\n", GetLastError());
-		return 1;
-	}
-	int Error = GetLastError();
-	int m;
-	char* buf = (char*)calloc(BUFFER_SIZE, sizeof(char));
-	DWORD nBytesRead;
-	while ((m = showMenu()) != 2) {
-		switch (m) {
-		case 1: {
-			DWORD write;
-			DWORD dwwr;
-			do
-			{
-				char name[255];
-				if (fgets(name, sizeof name, stdin) == NULL)
-					break;
-				if (strcmp(name, "\n") == 0)
-					break;
-				buf = strcat(buf, name);
-			} while (1);
-			if (WriteFile(hf, buf, strlen(buf), &dwwr, NULL) != 0)
-			{
-				cout << "Отправлено" << endl;
-				ReleaseSemaphore(hSemaphore, 1, NULL);
-			}
-			else {
-				cout << "Ошибка: не получилось отправить письмо!" << endl;
-			}
-			break;
-		}
-		}
+  while(1)
+  {
+   
+    fReturnCode = GetMailslotInfo(
+      hMailslot, NULL, &cbMessages,
+      &cbMsgNumber, NULL);
 
-	}
-	free(buf);
-	return (EXIT_SUCCESS);
+    if(!fReturnCode)
+    {
+      fprintf(stdout,"GetMailslotInfo: Error %ld\n", 
+        GetLastError());
+      getch();
+      break;
+    }
+
+    if(cbMsgNumber != 0)
+    {
+      if(ReadFile(hMailslot, szBuf, 512, &cbRead, NULL))
+      {
+        // ??????? ???????? ?????? ?? ??????? 
+        printf("Received: <%s>\n", szBuf);
+      
+        if(!strcmp(szBuf, "exit"))
+          break;
+      }
+      else
+      {
+        fprintf(stdout,"ReadFile: Error %ld\n", 
+          GetLastError());
+        getch();
+        break;
+      }
+    }
+    
+    Sleep(500);
+  }
+
+  CloseHandle(hMailslot);
+  return 0;
 }
-
